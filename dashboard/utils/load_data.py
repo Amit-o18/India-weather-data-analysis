98d16e27532a1e520d2fe04b672247b61976350b
Amit-o18 <amitkumar705911@gmail.com>
2026-07-18 15:07:14 +0530

    Improve Dataset Link

--- dashboard/utils/load_data.py
+++ dashboard/utils/load_data.py
@@ -4,5 +4,5 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv(r"C:\Users\hiii\OneDrive\Desktop\O7\Python\Project\data\cleaned_weather_data.csv")
+    df = pd.read_csv("data/cleaned_weather_data.csv")
     return df
